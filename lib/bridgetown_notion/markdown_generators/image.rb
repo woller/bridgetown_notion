53dc98ed0129440d0b8a29eef8648b90e8e1b23d
Nicolai Bach Woller <woller@traels.it>
2023-08-16 19:54:53 +0200

    Feature. Download images in image tags.

--- lib/bridgetown_notion/markdown_generators/image.rb
+++ lib/bridgetown_notion/markdown_generators/image.rb
@@ -4,19 +4,52 @@ module BridgetownNotion
   module MarkdownGenerators
     class Image < BridgetownNotion::MarkdownGenerators::Base
       def generate
+        download_file
+
         <<~MARKDOWN
           ![#{title}](#{url})
+
+          #{caption}
         MARKDOWN
       end
 
       private
 
       def title
-        "Untitled"
+        caption || "Untitled"
+      end
+
+      def caption
+        RichText.new(block["image"]["caption"])
       end
 
       def url
-        data["file"]["url"]
+        "/#{path}"
+      end
+
+      def path
+        "images/notion/#{block["id"]}.png"
+      end
+      
+      def download_file
+        destination_path = "src/#{path}"
+
+        # Create the destination directory if it doesn't exist
+        FileUtils.mkdir_p(File.dirname(destination_path))
+
+        uri = URI.parse(data["file"]["url"])
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+
+        # Download the image and save it to the destination path
+        begin
+          response = http.get(uri.request_uri)
+          open(destination_path, 'wb') do |destination|
+            destination.write(response.body)
+          end
+        rescue StandardError => e
+          puts "Error: #{e.message}"
+        end
       end
     end
   end
--- test/bridgetown_notion/markdown_generators/test_image.rb
+++ test/bridgetown_notion/markdown_generators/test_image.rb
@@ -4,16 +4,38 @@ require_relative "../../helper"
 
 class TestImage < Bridgetown::TestCase
   describe BridgetownNotion::MarkdownGenerators::Image do
+    before do
+      stub_request(:get, "https://example.com/image_url.png")
+        .with(headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent'=>'Ruby'
+        })
+        .to_return(status: 200, body: File.read("test/fixtures/ruby-logo.png"), headers: {})
+      @image = JSON.parse(File.read("test/fixtures/blocks/image.json"))
+    end
+
+    after do
+      FileUtils.rm_rf("src/")
+    end
+
     describe "#generate" do
       it "returns image representation of text" do
-        image = JSON.parse(File.read("test/fixtures/blocks/image.json"))
         expected_markdown = <<~MARKDOWN
-          ![Untitled](image_url.png)
+          ![Ruby Logo](/images/notion/b3863426-e252-49d6-ae76-8cffbcb242fa.png)
+
+          Ruby Logo
         MARKDOWN
 
-        result = BridgetownNotion::MarkdownGenerators::Image.new(image).generate
+        result = BridgetownNotion::MarkdownGenerators::Image.new(@image).generate
         
         assert_equal expected_markdown, result
+      end
+
+      it "saves image to src/images/notion" do
+        BridgetownNotion::MarkdownGenerators::Image.new(@image).generate
+
+        assert File.exist?("src/images/notion/b3863426-e252-49d6-ae76-8cffbcb242fa.png")
       end
     end
   end
